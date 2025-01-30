8a7268b04f60c76f68f51b548c140e24be6f9b18
Oliver Lau <oliver@ersatzworld.net>
2025-01-30 07:55:51 +0100

    Improved development web server: serves with no-cache policy; automatically opens default browsers with served URL.

--- _bin/serve.py
+++ _bin/serve.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python3
 
-from http.server import HTTPServer, SimpleHTTPRequestHandler
+from http.server import ThreadingHTTPServer, SimpleHTTPRequestHandler
 import os
+import socket
+import webbrowser
+import threading
+import time
 
-DIRECTORY_TO_SERVE = '.' 
-PORT = 3333
+DIRECTORY_TO_SERVE = os.getcwd()
+STARTING_PORT = 3333
 
-class MyHandler(SimpleHTTPRequestHandler):
+class DevHandler(SimpleHTTPRequestHandler):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.directory = DIRECTORY_TO_SERVE
@@ -15,13 +19,41 @@ class MyHandler(SimpleHTTPRequestHandler):
         pass  # Override to suppress logging    
 
     def translate_path(self, path):
-        path = path.split('?', 1)[0]
-        path = path.split('#', 1)[0]
-        path = os.path.join(self.directory, *path.split('/'))
+        path = path.split("?", 1)[0]
+        path = path.split("#", 1)[0]
+        path = os.path.join(self.directory, *path.split("/"))
         return path
+    
+    def end_headers(self):
+        self.send_header("Cache-Control", "no-store, no-cache, must-revalidate, max-age=0")
+        self.send_header("Pragma", "no-cache")
+        self.send_header("Expires", "0")
+        super().end_headers()
 
-if __name__ == '__main__':
-    with HTTPServer(('', PORT), MyHandler) as httpd:
-        print(f"Serving \x1b[1;31mNumemory\x1b[0m at http://localhost:{PORT}")
-        print("Logging disabled. To re-enable comment out `MyHandler.log_message()`")
+def find_free_port(start_port):
+    port = start_port
+    while port < 65536:
+        try:
+            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+                s.bind(("", port))
+                return port
+        except OSError:
+            port += 1
+
+def open_browser(url):
+    # Give the server a moment to start
+    time.sleep(0.5)
+    webbrowser.open(url)
+
+if __name__ == "__main__":
+    port = find_free_port(STARTING_PORT)
+    address = "127.0.0.1"
+    url = f"""http://{address}:{port}"""
+
+    threading.Thread(target=open_browser, args=(url,), daemon=True).start()
+    
+    with ThreadingHTTPServer((address, port), DevHandler) as httpd:
+        print(f"""Serving {DIRECTORY_TO_SERVE} at
+http://{httpd.server_address[0]}:\x1b[1;32m{httpd.server_address[1]}\x1b[0m""")
+        print("Logging disabled. To re-enable comment out `DevHandler.log_message()`")
         httpd.serve_forever()
